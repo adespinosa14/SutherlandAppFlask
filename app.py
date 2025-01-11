ceab148039044db725da92b2cf405f5aa472242e
Andrew Espinosa <adespinosa2002@gmail.com>
2025-01-11 12:04:35 -0800

    Create Database with events table

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ website's main architecture
 
 """
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQL_TRACK_MODIFICATION'] = False
 
 @app.route("/Home")
 @app.route("/")
@@ -35,6 +38,21 @@ def AddGames():
 def AddSounds():
     return render_template("Games/AddSounds.html")
 
+#Database Table Model
+db = SQLAlchemy(app)
+class Events(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Event_Name = db.Column(db.String(80), nullable = False)
+    Event_Description = db.Column(db.String(120), nullable = False)
+    Event_Location  = db.Column(db.String(120), nullable = False)
+    Event_Date = db.Column(db.Date(), nullable = False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
 # Run Program
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run()
