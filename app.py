1c1c30da686537924ba00e9062093973dec53216
Andrew Espinosa <adespinosa2002@gmail.com>
2025-01-11 18:21:54 -0800

    Finished Event List Functionality

--- app.py
+++ app.py
@@ -4,28 +4,83 @@ This file has the website backend routes, form handling, and anything else for t
 website's main architecture
 
 """
-from flask import Flask, render_template
+import uuid
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-app.config['SQL_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Taffy2002!!!@localhost:3306/Event_Sound_Data'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
+####### Database Table Model
+db = SQLAlchemy(app)
+
+# Event
+class Events(db.Model):
+    id = db.Column(db.String(80), primary_key=True)
+    Event_Name = db.Column(db.String(80), nullable = False)
+    Event_Description = db.Column(db.String(120), nullable = False)
+    Event_Location  = db.Column(db.String(120), nullable = False)
+    Event_Date = db.Column(db.Date(), nullable = False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+# Game Sounds
+class Game_Sounds(db.Model):
+    id = db.Column(db.String(80), primary_key=True)
+    Sound = db.Column(db.File)
+
+####### Routes
 @app.route("/Home")
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Event Routes
+####### Event Routes
 @app.route("/CurrentEvents")
 def CurrentEvents():
-    return render_template("Events/ViewEvents.html")
+    events = Events.query.all()
+    return render_template("Events/ViewEvents.html", events=events)
 
 @app.route("/ViewEvents/AddEvent")
 def AddEvent():
-    return render_template("Events/AddEvent.html")
+    my_uuid = uuid.uuid4()
+    return render_template("Events/AddEvent.html", uuid=my_uuid)
 
-# Game Routes
+####### Add Event
+@app.route("/ViewEvents/AddEvent/add-event", methods=['GET', 'POST'])
+def add_event():
+    if request.method == 'POST':
+            eventID = request.form["eventID"]
+            eventName = request.form['eventName']
+            eventDescription = request.form['eventDescription']
+            eventLocation = request.form['eventLocation']
+            eventDate = request.form['eventDate']
+
+            print(f"Event ID: {eventID}\nEvent Name: {eventName}\nEvent Description: {eventDescription}\nEvent Location: {eventLocation}\nEvent Date: {eventDate}")
+
+            new_event = Events(id = eventID, 
+                               Event_Name = eventName, 
+                               Event_Description = eventDescription, 
+                               Event_Location = eventLocation,
+                               Event_Date = eventDate)
+
+            db.session.add(new_event)
+            db.session.commit()
+            return redirect(url_for('CurrentEvents'))
+
+    return redirect(url_for('AddEvent'))
+####### Delete Event
+@app.route("/ViewEvents/DeleteEvent/<string:event_id>", methods=['POST'])
+def delete_event(event_id):
+    event = Events.query.get(event_id)
+    if event:
+        db.session.delete(event)
+        db.session.commit()
+        return redirect(url_for('CurrentEvents'))
+    return "Event not found", 404
+
+####### Game Routes
 @app.route("/ViewGames")
 def ViewGames():
     return render_template("Games/ViewGames.html")
@@ -36,21 +91,35 @@ def AddGames():
 
 @app.route("/ViewGames/AddSounds")
 def AddSounds():
-    return render_template("Games/AddSounds.html")
+    myuuid = uuid.uuid4()
+    return render_template("Games/AddSounds.html", uuid=myuuid)
 
-#Database Table Model
-db = SQLAlchemy(app)
-class Events(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    Event_Name = db.Column(db.String(80), nullable = False)
-    Event_Description = db.Column(db.String(120), nullable = False)
-    Event_Location  = db.Column(db.String(120), nullable = False)
-    Event_Date = db.Column(db.Date(), nullable = False)
+@app.route("/ViewGames/AddSounds/add_sound", methods=['POST'])
+def add_sound():
+    if request.method == 'POST':
+        id = request.form['Id']
+        sound = request.form['Sound']
+        print(f"Id: {id}\nSound: {sound}")
+        return redirect(url_for('AddGames'))
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+####### API calls to retrieve Event + Sound Data
+@app.route("/api/events")
+def retrieve_events():
+    events = Events.query.all()
+    event_list = [event_to_dict(event) for event in events]
+    return  jsonify(event_list)
 
-# Run Program
+####### Convert Events to a Dictionary
+def event_to_dict(event):
+    return {
+        "id": event.id,
+        "Event_Name": event.Event_Name,
+        "Event_Description": event.Event_Description,
+        "Event_Location": event.Event_Location,
+        "Event_Date": event.Event_Date.strftime("%Y-%m-%d")
+    }
+
+####### Run Program
 with app.app_context():
     db.create_all()
 
